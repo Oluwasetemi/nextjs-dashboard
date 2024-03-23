22a9a0d5c64a4fe5f23f4ae675f87cf2e5c0b961
Oluwasetemi Ojo <setemiojo@gmail.com>
2024-03-23 17:48:07 -0500

    Add revenue chart to dashboard page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,17 +9,19 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache'
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
+  noStore()
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
